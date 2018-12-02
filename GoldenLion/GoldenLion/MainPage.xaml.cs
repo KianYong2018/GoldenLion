c4ee6b76c2daa600a77032176e3032391f9c647c
Ng Kian Yong <LightingLiger@outlook.com>
2018-12-02 12:52:51 +0800

    Add project files.

---
+++ GoldenLion/GoldenLion/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GoldenLion
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var entry = new Entry { Placeholder = "Username", PlaceholderColor = Color.Olive };
+
+        }
+
+        private void ButtonEntry_Clicked(object sender, EventArgs e)
+        {
+            if(EntryUsername.Text != null && EntryPassword.Text != null)
+            {
+                LabelToast.Text = "Logining in. Please wait";
+            }
+            else
+            {
+                LabelToast.Text = "Do not leave any blanks";
+            }
+        }
+
+        async void ButtonSignUp_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUp());
+        }
+    }
+}
